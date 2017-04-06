48bc5dc1506a143e0c91bcf7f4cc0ac4afb9803b
max <max.huang@aihuishou.com>
2017-04-06 18:33:52 +0800

    update  ip filter of gii&debug modules

--- config/web.php
+++ config/web.php
@@ -67,13 +67,13 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => yii\debug\Module::class,
-        'allowedIPs' => ['127.0.0.1', '::1', '192.168.99.*'],
+        'allowedIPs' => ['127.0.0.1', '::1', '192.168.99.*', '172.17.0.*'],
     ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => yii\gii\Module::class,
-        'allowedIPs' => ['127.0.0.1', '::1', '192.168.99.*'],
+        'allowedIPs' => ['127.0.0.1', '::1', '192.168.99.*', '172.17.0.*'],
     ];
 }
 
