72e5e3bb70cb7a6ff463519af0b278c4e00d6812
taobig <studentht@hotmail.com>
2017-11-25 12:54:27 +0800

    update console config

--- _docker/_start_docker.sh
+++ _docker/_start_docker.sh
@@ -13,5 +13,12 @@ sed -i -e "s/die;//" ${app_path}/web/index.php;
 sed -i -e "s/\/\/dev //" ${app_path}/web/index.php;
 sed -i -e "s/\/\/dev //" ${app_path}/web/index.php;
 
+cp ${app_path}/yii-example ${app_path}/yii;
+sed -i -e "s/die;//" ${app_path}/yii;
+sed -i -e "s/\/\/dev //" ${app_path}/yii;
+sed -i -e "s/\/\/dev //" ${app_path}/yii;
+chmod +x ${app_path}/yii;
+
+
 #docker-compose exec -u www web composer install;
 docker-compose exec -u www web bash -c "composer global require 'fxp/composer-asset-plugin:^1.4.2';composer install";
