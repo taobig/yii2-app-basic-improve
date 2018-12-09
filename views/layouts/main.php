df086aad3e11849ebf9d9d5ca397abafa736acf8
taobig <studentht@hotmail.com>
2018-12-09 23:03:00 +0800

    移除Yii::powered()调用

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -91,8 +91,6 @@ StaticResource::loadScript($this, 'scripts/common.js');
 <footer class="footer">
     <div class="container">
         <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
 </footer>
 
