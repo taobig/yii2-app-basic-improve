9a163b11fb67f8803c610220486bfacc35a153d0
taobig <studentht@hotmail.com>
2018-12-09 21:15:34 +0800

    add db comments

--- migrations/m181201_010101_init.php
+++ migrations/m181201_010101_init.php
@@ -19,15 +19,15 @@ class m181201_010101_init extends Migration
         $datetime = date('Y-m-d H:i:s');
         $sql = "
             CREATE TABLE `{$tableName}` (
-                `id` int(11) NOT NULL AUTO_INCREMENT,
-                `account` varchar(20) NOT NULL,
-                `nickname` varchar(20) NOT NULL,
-                `password` varchar(100) NOT NULL,
-                `active` tinyint(4) NOT NULL,
-                `dt_created` datetime NOT NULL,
-                `dt_updated` datetime DEFAULT NULL,
-                PRIMARY KEY (`id`)
-                ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+              `id` int(11) NOT NULL AUTO_INCREMENT,
+              `account` varchar(20) NOT NULL COMMENT '用户名',
+              `nickname` varchar(20) NOT NULL COMMENT '昵称',
+              `password` varchar(100) NOT NULL COMMENT '密码',
+              `active` tinyint(4) NOT NULL COMMENT '状态1-有效 0-无效',
+              `dt_created` datetime NOT NULL COMMENT '创建时间',
+              `dt_updated` datetime DEFAULT NULL COMMENT '最后更新时间',
+              PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
             
             INSERT INTO `{$tableName}` (`account`, `nickname`, `password`, `active`, `dt_created`)
             VALUES ('{$username}', '{$username}', '{$hashedPassword}', '1', '{$datetime}');
