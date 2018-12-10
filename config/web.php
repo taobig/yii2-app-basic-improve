34024bbd3889de7fa7016752f08ab5ef1d3c44c3
taobig <studentht@hotmail.com>
2018-12-10 09:30:43 +0800

    add yiisoft/yii2-debug for dev

--- config/web.php
+++ config/web.php
@@ -77,11 +77,11 @@ $config = [
 
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
-//    $config['bootstrap'][] = 'debug';
-//    $config['modules']['debug'] = [
-//        'class' => yii\debug\Module::class,
-//        'allowedIPs' => ['127.0.0.1', '::1', '192.168.*.*'],
-//    ];
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => yii\debug\Module::class,
+        'allowedIPs' => ['127.0.0.1', '::1', '192.168.*.*'],
+    ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
