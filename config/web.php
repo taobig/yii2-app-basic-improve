d61543b2a183245a74d0fea670fe1002393c9e89
taobig <studentht@hotmail.com>
2019-01-03 18:04:09 +0800

    开发环境下，不再限制使用yii-debug&yii-gii的来源ip

--- config/web.php
+++ config/web.php
@@ -87,13 +87,13 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => yii\debug\Module::class,
-        'allowedIPs' => ['127.0.0.1', '::1', '192.168.*.*'],
+        'allowedIPs' => ['127.0.0.1', '::1', '192.168.*.*', '*'],
     ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => yii\gii\Module::class,
-        'allowedIPs' => ['127.0.0.1', '::1', '192.168.*.*'],
+        'allowedIPs' => ['127.0.0.1', '::1', '192.168.*.*', '*'],
     ];
 }
 
