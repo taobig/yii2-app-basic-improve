7d091a9445de4252d1d2b0b1b9bd428079fad980
taobig <studentht@hotmail.com>
2019-01-06 23:07:47 +0800

    revert BaseModel::delete

--- models/BaseModel.php
+++ models/BaseModel.php
@@ -7,19 +7,10 @@ use yii\db\ActiveRecord;
 class BaseModel extends ActiveRecord
 {
 
-    protected static $softDeleteAttribute = 'deleted_at';
-
     public function optimisticLock()
     {
         return 'version';
     }
 
-    public function delete()
-    {
-        if (static::$softDeleteAttribute) {
-            $this->{static::$softDeleteAttribute} = time();
-            return $this->update(true, [static::$softDeleteAttribute]);
-        }
-    }
 
 }
