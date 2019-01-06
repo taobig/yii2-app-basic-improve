46f06ffd333038087bd9dd7ff282324da9ac0588
taobig <studentht@hotmail.com>
2019-01-06 13:13:41 +0800

    update db config

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => yii\db\Connection::class,
     'dsn' => 'mysql:host=localhost;dbname=yii2-app-basic-improve',
     'tablePrefix' => 'tbl_',
-    'username' => 'root',
+    'username' => 'test',
     'password' => '******',
     'charset' => 'utf8mb4',
 ];
