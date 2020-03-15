3a07652bc099e9d059e6962361a3c89356d2faef
taobig <studentht@hotmail.com>
2020-03-15 16:32:03 +0800

    add BaseEnum::dropdownList

--- enums/BaseEnum.php
+++ enums/BaseEnum.php
@@ -8,6 +8,19 @@ abstract class BaseEnum
 
     abstract public static function labels(): array;
 
+    public static function dropdownList($defaultValue = null, string $name = '----'): array
+    {
+        if ($defaultValue !== null) {
+            $result[$defaultValue] = $name;
+            foreach (static::labels() as $key => $val) {
+                $result[$key] = $val;
+            }
+            return $result;
+        } else {
+            return static::labels();
+        }
+    }
+
     public static function label($val)
     {
         return static::labels()[$val] ?? $val;
