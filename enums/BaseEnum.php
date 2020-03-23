92aefd6628100c741776bdb93481177319bc93b3
taobig <studentht@hotmail.com>
2020-03-23 21:28:36 +0800

    BaseEnum extends EnumBase

--- enums/BaseEnum.php
+++ enums/BaseEnum.php
@@ -2,33 +2,9 @@
 
 namespace app\enums;
 
+use taobig\helpers\base\EnumBase;
 
-abstract class BaseEnum
+abstract class BaseEnum extends EnumBase
 {
-
-    abstract public static function labels(): array;
-
-    public static function dropdownList($defaultValue = null, string $name = '----'): array
-    {
-        if ($defaultValue !== null) {
-            $result[$defaultValue] = $name;
-            foreach (static::labels() as $key => $val) {
-                $result[$key] = $val;
-            }
-            return $result;
-        } else {
-            return static::labels();
-        }
-    }
-
-    public static function label($val)
-    {
-        return static::labels()[$val] ?? $val;
-    }
-
-    public static function exists($va): bool
-    {
-        return isset(static::labels()[$va]);
-    }
 
 }
